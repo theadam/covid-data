8f8f4357fb53d168bf9d473a4154d04a484b8ea8
Adam Nalisnick <theadam4257@gmail.com>
2020-03-23 00:01:32 -0400

    Allow JSON serialization

--- data/models.go
+++ data/models.go
@@ -7,26 +7,26 @@ import (
 
 type DataPoint struct {
     gorm.Model
-    State string
-    Country string
-    Confirmed int
-    Deaths int
-    Recovered int
-    Date time.Time
-    Lat string
-    Long string
+    State string `json:"state"`
+    Country string `json:"country"`
+    Confirmed int `json:"confirmed"`
+    Deaths int `json:"deaths"`
+    Recovered int `json:"recovered"`
+    Date time.Time `json:"date"`
+    Lat string `json:"lat"`
+    Long string `json:"long"`
 }
 
 var Point DataPoint
 
 type CountyData struct {
     gorm.Model
-    ExternalId string
-    State string
-    County string
-    Confirmed int
-    Deaths int
-    Date time.Time
+    ExternalId string `json:"externalId"`
+    State string `json:"state"`
+    County string `json:"county"`
+    Confirmed int `json:"confirmed"`
+    Deaths int `json:"deaths"`
+    Date time.Time `json:"date"`
 }
 
 var CountyCases CountyData

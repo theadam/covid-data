cf17d6c81524408d19c7918444181d6038bbb5a9
Adam Nalisnick <theadam4257@gmail.com>
2020-04-02 00:49:57 -0400

    Support heroku

--- utils/db.go
+++ utils/db.go
@@ -2,23 +2,30 @@ package utils
 
 import (
 	"fmt"
+	"os"
+	"time"
+
 	"github.com/jinzhu/gorm"
-    _ "github.com/jinzhu/gorm/dialects/postgres"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
-const waittime = 40
+const waittime = 10
 
 func OpenDB() *gorm.DB {
     cur := 0
     var db *gorm.DB
     var err error
+
     for (err != nil || db == nil) && cur < waittime {
-        db, err := gorm.Open("postgres", "host=postgres user=postgres dbname=covid password=password sslmode=disable")
+        db, err = gorm.Open("postgres", os.Getenv("DATABASE_URL") + "?sslmode=disable")
         if err == nil {
             db.LogMode(true)
             return db
         }
+        fmt.Println("Failed to connect to database... Trying again.")
+        time.Sleep(1 * time.Second)
+        cur++
     }
-    fmt.Println(err)
+    fmt.Println(err.Error())
     panic("failed to connect database")
 }

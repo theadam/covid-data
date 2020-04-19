4f5c43dca8ca7d07dbff9479f5156d61b21e047b
Adam Nalisnick <theadam4257@gmail.com>
2020-04-18 21:50:54 -0400

    Goroutines for files

--- cmd/loader/main.go
+++ cmd/loader/main.go
@@ -4,26 +4,36 @@ import (
 	"covid-tracker/data"
 	"covid-tracker/fetch/jhu"
 	"fmt"
+	"sync"
 	"time"
 )
 
-func runAction(name string, action func()) {
+func runAction(wg *sync.WaitGroup, name string, action func()) {
+    wg.Add(1)
 	now := time.Now()
-	fmt.Println("Starting " + name)
-	action()
-	fmt.Println("Finished " + name + " in " + time.Since(now).String())
-	fmt.Println()
+
+    go func () {
+        action()
+        fmt.Println("Finished " + name + " in " + time.Since(now).String())
+        fmt.Println()
+        wg.Done()
+    }()
 }
 
 func main() {
+	now := time.Now()
 	fmt.Println("Loading all data")
 	fmt.Println()
 
 	points := jhu.GetData()
 
-	runAction("Writing World JSON data", func() { data.WriteWorldData(points) })
-	runAction("Writing Province JSON data", func() { data.WriteProvinceData(points) })
-	runAction("Writing State JSON data", func() { data.WriteStateData(points) })
-	runAction("Writing County JSON data", func() { data.WriteCountyData(points) })
-	runAction("Writing Date Range JSON data", func() { data.WriteDateRange(points) })
+	var wg sync.WaitGroup
+
+	runAction(&wg, "Writing World JSON data", func() { data.WriteWorldData(points) })
+	runAction(&wg, "Writing Province JSON data", func() { data.WriteProvinceData(points) })
+	runAction(&wg, "Writing State JSON data", func() { data.WriteStateData(points) })
+	runAction(&wg, "Writing County JSON data", func() { data.WriteCountyData(points) })
+	runAction(&wg, "Writing Date Range JSON data", func() { data.WriteDateRange(points) })
+    wg.Wait()
+    fmt.Println("Finished in " + time.Since(now).String())
 }

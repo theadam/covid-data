ae9f34901629a435be642a4bb5fa6028c3aa74f0
Adam Nalisnick <theadam4257@gmail.com>
2020-05-12 23:33:35 +0000

    Updates

---
+++ precache-manifest.a53e8d5d915cb8181603415f1cbd414e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8229abc66a6619f4e0322089f44ab5ea",
+    "url": "/covid-data/index.html"
+  },
+  {
+    "revision": "34a82747698f8703a6a2",
+    "url": "/covid-data/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "75162da827b3b964692d",
+    "url": "/covid-data/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "34a82747698f8703a6a2",
+    "url": "/covid-data/static/js/2.1360c420.chunk.js"
+  },
+  {
+    "revision": "be0b74659518ec817941c420332039fd",
+    "url": "/covid-data/static/js/2.1360c420.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75162da827b3b964692d",
+    "url": "/covid-data/static/js/main.7b09a31b.chunk.js"
+  },
+  {
+    "revision": "d1232fd224ec82577533",
+    "url": "/covid-data/static/js/runtime-main.d5d460c9.js"
+  }
+]);
